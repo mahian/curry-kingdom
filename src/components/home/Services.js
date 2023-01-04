c533dd428aae6bdba7599902e3eb77ac4a52c448
mahian ahmad <mahianahamd9@gmail.com>
2023-01-04 16:18:40 +0600

    customize navbar

--- src/components/home/Services.js
+++ src/components/home/Services.js
@@ -13,10 +13,10 @@ const Services = () => {
                         <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Master Cleanse Reliac Heirloom</h1>
                     </div> */}
                     <div class="flex flex-wrap -m-4">
-                        <div class="p-4 md:w-1/3">
-                            <div class="flex rounded-lg h-full p-8 flex-col shadow-lg">
+                        <div class="p-4 md:w-1/3 ">
+                            <div class="flex rounded-lg h-full p-8 flex-col shadow-lg service_card">
                                 <div class="mb-3">
-                                    <div class="text-primary">
+                                    <div class="text-primary icon">
                                         <IoFastFoodOutline class="w-10 h-10"/>
                                     </div>
                                     <h2 class="text-gray-900 font-bold text-3xl mt-3 title-font">Delicious Food</h2>
@@ -26,11 +26,11 @@ const Services = () => {
                                 </div>
                             </div>
                         </div>
-                        <div class="p-4 md:w-1/3">
-                            <div class="flex rounded-lg h-full p-8 flex-col shadow-lg">
+                        <div class="p-4 md:w-1/3 ">
+                            <div class="flex rounded-lg h-full p-8 flex-col shadow-lg service_card">
                                 <div class="mb-3">
-                                    <div class="text-primary">
-                                        <GiPartyPopper class="w-10 h-10"/>
+                                    <div class="text-primary icon">
+                                        <GiPartyPopper class="w-10 h-10 "/>
                                     </div>
                                     <h2 class="text-gray-900 font-bold text-3xl mt-3 title-font">Fun Time</h2>
                                 </div>
@@ -39,11 +39,11 @@ const Services = () => {
                                 </div>
                             </div>
                         </div>
-                        <div class="p-4 md:w-1/3">
-                            <div class="flex rounded-lg h-full p-8 flex-col shadow-lg">
+                        <div class="p-4 md:w-1/3 ">
+                            <div class="flex rounded-lg h-full p-8 flex-col shadow-lg service_card">
                                 <div class="mb-3">
-                                    <div class="text-primary">
-                                        <DiYii class="w-10 h-10"/>
+                                    <div class="text-primary icon">
+                                        <DiYii class="w-10 h-10 "/>
                                     </div>
                                     <h2 class="text-gray-900 font-bold text-3xl mt-3 title-font">Healthy Options</h2>
                                 </div>
